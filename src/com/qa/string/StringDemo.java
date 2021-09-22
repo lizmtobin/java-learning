ccc5c1a449b2e436ee4d9e1bd347eeee5b9d70d1
liz tobin <lizmtobin@gmail.com>
2021-09-22 10:18:35 +0100

    length and concat string practice

--- src/com/qa/string/StringDemo.java
+++ src/com/qa/string/StringDemo.java
@@ -16,12 +16,49 @@ public class StringDemo {
         String str2 = "I like strings";
         System.out.println(str1 == str2); // output will be: true
 
-        // Method 2
+        // Method 2 /// equal for two identical string objects
         
         String str3 = new String("I like strings");
         String str4 = new String("I like strings");
         System.out.println(str3 == str4); // output will be: false
         System.out.println(str3.equals(str4));  // output will be: true
+        
+        //Making strings from array
+        
+        char[] helloArray = { 'H', 'e', 'l', 'l', 'o', '.' }; // an array of chars
+        String helloString = new String(helloArray);  
+        System.out.println(helloString); // prints out Hello.
+        
+        //Checking length
+        String myStr = "I like strings";
+        int stringLength = myStr.length();
+        System.out.println("String Length is " + stringLength);
+        // output will be: String Length is 14
+        
+        //concatination 
+        
+        String myStr3 = "My cat's name is ".concat("Copycat");
+        System.out.println(myStr3);
+        // output will be: My cat's name is Copycat
+
+        //example 2
+        String myStr8 = "Hello, " + "World" + "!" ;
+        System.out.println(myStr8);
+        // output will be: Hello, World!
+
+        //example 2.5
+        String myStr5 = "Hello, "+ "World" + "!";
+        System.out.println(myStr5);
+        //output will be: Hello, World!
+
+        //example 3
+        String string1 = "Welcome ";
+        String string2 = "to ";
+        String string3 = "Java";
+        String string4 = "!";
+        System.out.println(string1 + string2 + string3 + string4);
+        // output will be: Welcome to Java!
+        
 	}
 
 }
